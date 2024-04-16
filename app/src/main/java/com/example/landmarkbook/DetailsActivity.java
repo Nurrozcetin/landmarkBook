97ac4b4ca8acbae4c644ebdadf3f4b4f5c6673fc
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-04-16 13:43:21 +0300

    use of binding

--- app/src/main/java/com/example/landmarkbook/DetailsActivity.java
+++ app/src/main/java/com/example/landmarkbook/DetailsActivity.java
@@ -1,6 +1,8 @@
 package com.example.landmarkbook;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.landmarkbook.databinding.ActivityDetailsBinding;
+
 public class DetailsActivity extends AppCompatActivity {
+    private ActivityDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater()); //INFLATE LAYOUT ILE KODUN BAGLANMASINI SAGLAR
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
 }
--- app/src/main/java/com/example/landmarkbook/MainActivity.java
+++ app/src/main/java/com/example/landmarkbook/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.landmarkbook;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.landmarkbook.databinding.ActivityDetailsBinding;
+import com.example.landmarkbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Landmark> landmarkArrayList;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); //INFLATE LAYOUT ILE KODUN BAGLANMASINI SAGLAR
+        View view = binding.getRoot();
+        setContentView(view);
+
+        landmarkArrayList = new ArrayList<>();
+
+        Landmark pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
+        Landmark londonBridge = new Landmark("London Bridge", "UK", R.drawable.london);
+        Landmark eiffel = new Landmark("Eiffel", "France", R.drawable.eyfel);
+        Landmark collosseum = new Landmark("Collosseum", "Italy",R.drawable.collesium);
+
+        landmarkArrayList.add(pisa);
+        landmarkArrayList.add(londonBridge);
+        landmarkArrayList.add(eiffel);
+        landmarkArrayList.add(collosseum);
+
+        // Listview ile birlikte ArrayAdapter kullanimi verimsiz ama daha kolaydir
     }
 }
